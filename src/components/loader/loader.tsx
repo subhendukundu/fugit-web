47996c7e5b132f635cd491abc4264e46e224c3f9
Subhendu Kundu <subhendukundu@Subhendus-MacBook-Pro.local>
2023-03-20 01:30:47 +0530

    fix: trying lottie

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,10 +1,52 @@
 import {
   component$,
+  noSerialize,
+  useSignal,
+  useStore,
+  useVisibleTask$,
 } from "@builder.io/qwik";
+import lottie from "lottie-web";
+
+export interface Options {
+  container?: any;
+  renderer?: "svg";
+  loop?: boolean;
+  autoplay?: boolean;
+  animationData?: object;
+  path?: string;
+  rendererSettings?: object;
+  name?: string;
+}
+
+const options: Options = {
+  path: "/loading-screen.json",
+};
 
 export default component$(() => {
+  const store = useStore({
+    anim: noSerialize({}),
+  });
+
+  const canvas = useSignal<HTMLElement>();
+
+  useVisibleTask$(() => {
+    store.anim = noSerialize(
+      lottie.loadAnimation({
+        container: options.container || canvas.value,
+        renderer: options.renderer || "svg",
+        loop: options.loop || true,
+        autoplay: options.autoplay || true,
+        animationData: options.animationData,
+        path: options.path,
+        rendererSettings: options.rendererSettings,
+        name: options.name,
+      })
+    );
+  });
+
   return (
     <div class="flex flex-col items-center justify-center">
+      <div class="max-w-sm" ref={canvas}></div>
       <p class="text-gray-500 text-sm mt-2">Loading...</p>
     </div>
   );
