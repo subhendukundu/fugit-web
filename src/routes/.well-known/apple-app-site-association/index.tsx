dd5a7ec77f717671b6c61dbe97bdad6844a34a16
Subhendu Kundu <subhendukundu14@gmail.com>
2023-11-03 19:10:19 +0530

    fix: added well-knowns

--- src/routes/.well-known/apple-app-site-association/index.tsx
+++ src/routes/.well-known/apple-app-site-association/index.tsx
@@ -4,15 +4,13 @@ export const onGet: RequestHandler = async ({ json, headers }) => {
   headers.set("Content-Type", "application/json");
   json(200, {
     applinks: {
-      apps: ["F5SZLQKVH8.io.higgle.fugitapp"],
-      details: {
-        appIDs: ["F5SZLQKVH8.io.higgle.fugitapp"],
-        appID: "F5SZLQKVH8.io.higgle.fugitapp",
-        paths: ["*"],
-      },
-    },
-    webcredentials: {
-      apps: ["F5SZLQKVH8.io.higgle.fugitapp"],
+      apps: [],
+      details: [
+        {
+          appID: "F5SZLQKVH8.io.higgle.fugitapp",
+          paths: ["*"],
+        },
+      ],
     },
   });
 };
